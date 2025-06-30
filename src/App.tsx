64ca581458a76e42051f5da9a81400af5a39cfaa
kalgrinberg <kal@factory.ai>
2025-06-29 21:59:55 -0700

    Add metadata to Product Cards.  Add primary button.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import './App.css';
 import './styles.css';
 import Hero from './assets/components/hero';
 import TopNav from "./assets/components/topnav";
-import ProductCard from './assets/components/product-card';
 import Carousel from './assets/components/carousel';
 
 function App() {
@@ -13,6 +12,8 @@ function App() {
       <TopNav />
       
       <Hero />
+
+      <Carousel />
 
       <Carousel />
 
--- src/assets/components/product-card.tsx
+++ src/assets/components/product-card.tsx
@@ -5,9 +5,15 @@ import Multicolor from "../images/SodiumBracelet_01_MultiColor.png";
 function ProductCard() {
     return (
         <div className="card-container">
+
             <div className="color-style"> Multi Color </div>
             <div className="color-stripe"> </div>
             <img className="product" src={Multicolor} />
+            <div className="material"> 18K White Gold & Diamonds </div>
+            <div className="AddtoCart"> 
+                <span>Add to Cart</span>
+                <span>$5,940.00</span>
+            </div>
 
         </div>
     )
