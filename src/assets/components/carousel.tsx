24db1e3de5b8522c4ceae4df9b3f512294ec54af
kalgrinberg <kal@factory.ai>
2025-06-29 22:56:18 -0700

    Componentize product card names, images, etc.

--- src/assets/components/carousel.tsx
+++ src/assets/components/carousel.tsx
@@ -1,14 +1,26 @@
 import ProductCard from "./product-card";
 import "./carousel.css";
-
+import Multicolor from "../images/SodiumBracelet_01_MultiColor.png";
+import Clover from "../images/SodiumBracelet_GradientClover.png";
+import Retba from "../images/SodiumBracelet_GradientRetbaPink.png";
+import ChetBlue from "../images/SodiumBracelet_GradientChetBlue.png";
 
 function Carousel() {
     return (
 
         <div className="carouselContainer">
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
+            <ProductCard
+                colorStyle={"Clover Gradient"}
+                productImage={Clover}
+            />
+            <ProductCard 
+                colorStyle={"Retba Pink"} 
+                productImage={Retba}
+            />
+            <ProductCard 
+                colorStyle={"Chet Blue Gradient"}
+                productImage={ChetBlue}
+            />
         </div>
 
     );
--- src/assets/components/product-card.tsx
+++ src/assets/components/product-card.tsx
@@ -2,13 +2,13 @@ import "../../styles.css";
 import "./product-card.css";
 import Multicolor from "../images/SodiumBracelet_01_MultiColor.png";
 
-function ProductCard() {
+function ProductCard(props: any) {
     return (
         <div className="card-container">
 
-            <div className="color-style"> Multi Color </div>
+            <div className="color-style"> {props.colorStyle} </div>
             <div className="color-stripe"> </div>
-            <img className="product" src={Multicolor} />
+            <img className="product" src={props.productImage} />
             <div className="material"> 18K White Gold & Diamonds </div>
             <div className="AddtoCart"> 
                 <span>Add to Cart</span>
