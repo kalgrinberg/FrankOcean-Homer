5e08b1fc76d4e56231e46f093db0702fb50fbb65
kalgrinberg <kal@factory.ai>
2025-06-30 23:22:30 -0700

    Add scroll interaction on carousel.  Add gradient props to product cards

--- src/assets/components/carousel.tsx
+++ src/assets/components/carousel.tsx
@@ -1,26 +1,48 @@
 import ProductCard from "./product-card";
 import "./carousel.css";
-import Multicolor from "../images/SodiumBracelet_01_MultiColor.png";
 import Clover from "../images/SodiumBracelet_GradientClover.png";
 import Retba from "../images/SodiumBracelet_GradientRetbaPink.png";
 import ChetBlue from "../images/SodiumBracelet_GradientChetBlue.png";
+import BubbleBlue from "../images/SodiumBracelet_GradientBubbleBlue.png";
+import Cadmium from "../images/SodiumBracelet_GradientCadmiumRed.png";
+import Infrared from "../images/SodiumBracelet_GradientInfrared.png";
 
 function Carousel() {
     return (
 
         <div className="carouselContainer">
-            <ProductCard
-                colorStyle={"Clover Gradient"}
-                productImage={Clover}
-            />
-            <ProductCard 
-                colorStyle={"Retba Pink"} 
-                productImage={Retba}
-            />
-            <ProductCard 
-                colorStyle={"Chet Blue Gradient"}
-                productImage={ChetBlue}
-            />
+            <div className="carouselTrack">
+                <ProductCard
+                    colorStyle={"Clover Gradient"}
+                    productImage={Clover}
+                    colorStripe={{background: "linear-gradient(to right, rgb(0, 175, 102), rgb(192, 218, 99)" }}
+                />
+                <ProductCard
+                    colorStyle={"Retba Pink"}
+                    productImage={Retba}
+                    colorStripe={{background: "linear-gradient(to right, rgb(0, 175, 102), rgb(244, 166, 215)" }}
+                />
+                <ProductCard
+                    colorStyle={"Chet Blue Gradient"}
+                    productImage={ChetBlue}
+                    colorStripe={{background: "linear-gradient(to right, rgb(48, 127, 226), rgb(192, 218, 99)" }}
+                />
+                <ProductCard
+                    colorStyle={"Bubble Blue Gradient"}
+                    productImage={BubbleBlue}
+                    colorStripe={{background: "linear-gradient(to right, #59cbe8, #c0da63)" }}
+                />
+                <ProductCard
+                    colorStyle={"Cadmium Red Gradient"}
+                    productImage={Cadmium}
+                    colorStripe={{background: "linear-gradient(to right, #f9423a, #ff6720)" }}
+                />
+                <ProductCard
+                    colorStyle={"Infrared Gradient"}
+                    productImage={Infrared}
+                    colorStripe={{background: "linear-gradient(to right, #ff6720, #c0da63)" }}
+                />
+            </div>
         </div>
 
     );
--- src/assets/components/product-card.tsx
+++ src/assets/components/product-card.tsx
@@ -1,13 +1,12 @@
 import "../../styles.css";
 import "./product-card.css";
-import Multicolor from "../images/SodiumBracelet_01_MultiColor.png";
 
 function ProductCard(props: any) {
     return (
         <div className="card-container">
 
             <div className="color-style"> {props.colorStyle} </div>
-            <div className="color-stripe"> </div>
+            <div className="color-stripe" style={props.colorStripe}> </div>
             <img className="product" src={props.productImage} />
             <div className="material"> 18K White Gold & Diamonds </div>
             <div className="AddtoCart"> 
